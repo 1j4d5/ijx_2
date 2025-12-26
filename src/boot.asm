6da2b2aaeb43e767cbf761dd5c789479a8322dfa
1j4d5 <ijads.main@gmail.com>
2025-12-26 22:27:27 +0500

    creating an empty os base

--- src/boot.asm
+++ src/boot.asm
@@ -1,13 +1,13 @@
-org 0x7C00
-bits 16
+org 0x7C00                  ; setting origin to 0x7C00 
+bits 16                     ; 16-bit code
 
 
 
 main:
-    hlt
+    hlt                     ; halt the CPU  
   
 .halt:
-    jmp .halt
+    jmp .halt               ; infinite loop if the cpu resumes after hlt
 
 times 510-($-$$) db 0       ; 510-(size of the file) * 0 so it fills with 0s
 dw 0xAA55
